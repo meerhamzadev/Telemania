bb8efd84878df7f319e11cd403ab95cd9d005eef
MeerHamza1421 <hamzababar37@gmail.com>
2020-12-25 00:50:20 +0500

    _

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
+import React from 'react'
+import NavBar from './Components/Js-Files/navBar/NavBar';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import InTheaters from './Components/Js-Files/InTheators/InTheators';
+import HomePage from './Components/Js-Files/HomePage/HomePage';
+import ComingSoon from './Components/Js-Files/CommingSoon/ComingSoon';
+import MostPopularMovies from './Components/Js-Files/Popular/MostPopularMovies';
+import MostPopularSeries from './Components/Js-Files/Popular/MostPopularSeries';
+import Top250Series from './Components/Js-Files/Top250/Top250Series';
+import Top250Movies from './Components/Js-Files/Top250/Top250Movies';
+import ItemDetailPage from './Components/Js-Files/FilmDetails/ItemDetailPage';
+import ActorDetail from './Components/Js-Files/FilmDetails/ActorDetail';
+import WatchList from './Components/Js-Files/WatchList/WatchList';
+import SearchBox from './Components/Js-Files/SearchBox/SearchBox';
+import ErrorBoundary from './Components/Js-Files/ErrorBounderies/ErrorBoundary';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const App = () => (
+  <Router>
+    <NavBar />
+    <Switch>
+      <Route path="/" component={HomePage} exact />
+      <Route path="/_In_Theaters" component={InTheaters} exact />
+      <Route path="/_Coming_Soon" component={ComingSoon} exact />
+      <Route path="/_Trending_Movies" component={MostPopularMovies} exact />
+      <Route path="/_Trending_Series" component={MostPopularSeries} exact />
+      <Route path="/_Most_Rated_Movies" component={Top250Movies} exact />
+      <Route path="/_Most_Rated_Series" component={Top250Series} exact />
+      <Route path="/_movieWatchList" component={WatchList} exact />
+      <Route path="/searchedItems" component={SearchBox} />
+      {/* <ErrorBoundary> */}
+      <Route path="/:id" component={ItemDetailPage} exact />
+      {/* </ErrorBoundary> */}
+      <Route path="/Actor/:actorId" component={ActorDetail} exact />
+    </Switch>
+  </Router>
+);
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,10 +3,22 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WatchListContextProvider } from './Components/Js-Files/WatchList/WatchListContext';
+import { SearchBoxContext } from './Components/Js-Files/SearchBox/SearchBoxContext';
+import { DataFetchingContextProvider } from './Components/Js-Files/Shared_Component/DataFetchingContext';
+
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <DataFetchingContextProvider>
+      <WatchListContextProvider>
+        <SearchBoxContext>
+          <App />
+        </SearchBoxContext>
+      </WatchListContextProvider>
+
+    </DataFetchingContextProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
