73f68b756f7994a816d044712a67e3551f0b1809
MeerHamza1421 <hamzababar37@gmail.com>
2020-12-25 00:12:31 +0500

    For trending serires and movies

---
+++ src/Components/Js-Files/Popular/MostPopularMovies.js
@@ -0,0 +1,17 @@
+import React, { useContext } from 'react'
+import FullPageData from '../Shared_Component/FullPageData';
+import { FetchedDataContext } from '../Shared_Component/DataFetchingContext';
+
+const MostPopularMovies = () => {
+    const [FilmData, updateFilmData] = useContext(FetchedDataContext)
+
+    return (
+
+        <FullPageData
+            sectionTitle="Trending_Movies"
+            filmData={FilmData[2].items}
+        />
+    );
+}
+
+export default MostPopularMovies
