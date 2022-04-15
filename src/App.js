d4cb60545e56895c99162fe3b75c3bae59f108c2
meerhamzadev <hamzababar37@gmail.com>
2022-04-15 22:56:36 +0500

    ‼ Breaking: Moved to version 2.0

--- src/App.js
+++ src/App.js
@@ -1,43 +1,9 @@
 import React from 'react'
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import NavBar from './Components/navBar/NavBar';
-import InTheaters from './Components/InTheators/InTheators';
-import HomePage from './Components/HomePage/HomePage';
-import ComingSoon from './Components/CommingSoon/ComingSoon';
-import MostPopularMovies from './Components/Popular/MostPopularMovies';
-import MostPopularSeries from './Components/Popular/MostPopularSeries';
-import Top250Series from './Components/Top250/Top250Series';
-import Top250Movies from './Components/Top250/Top250Movies';
-import ItemDetailPage from './Components/FilmDetails/ItemDetails/ItemDetailPage';
-import ActorDetail from './Components/FilmDetails/ActorDetails/ActorDetail';
-import WatchList from './Components/WatchList/WatchList';
-import SearchBox from './Components/SearchBox/SearchBox';
-import ErrorBoundary from './Components/ErrorBounderies/ErrorBoundary';
-import Footer from './Components/Footer/Footer';
-import './App.css';
-
 
 const App = () => (
-  <Router>
-
-    <NavBar />
-    <Switch>
-      <Route path="/" component={HomePage} exact />
-      <Route path="/_In_Theaters" component={InTheaters} exact />
-      <Route path="/_Coming_Soon" component={ComingSoon} exact />
-      <Route path="/_Trending_Movies" component={MostPopularMovies} exact />
-      <Route path="/_Trending_Series" component={MostPopularSeries} exact />
-      <Route path="/_Most_Rated_Movies" component={Top250Movies} exact />
-      <Route path="/_Most_Rated_Series" component={Top250Series} exact />
-      <Route path="/_movieWatchList" component={WatchList} exact />
-      <Route path="/searchedItems" component={SearchBox} />
-      {/* <ErrorBoundary> */}
-      <Route path="/:id" component={ItemDetailPage} exact />
-      {/* </ErrorBoundary> */}
-      <Route path="/Actor/:actorId" component={ActorDetail} exact />
-    </Switch>
-    <Footer />
-  </Router>
+  <div>
+    <h1>Hello World</h1>
+  </div>
 );
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -2,28 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { WatchListContextProvider } from './Components/WatchList/WatchListContext';
-import { SearchBoxContext } from './Components/SearchBox/SearchBoxContext';
-import { DataFetchingContextProvider } from './Components/Shared_Component/DataFetchingContext';
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <DataFetchingContextProvider>
-      <WatchListContextProvider>
-        <SearchBoxContext>
-          <App />
-        </SearchBoxContext>
-      </WatchListContextProvider>
-
-    </DataFetchingContextProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
