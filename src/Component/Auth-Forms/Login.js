5f274a14084457263ea0ff851360cdf053db1000
meerhamzadev <hamzababar37@gmail.com>
2022-05-12 14:41:56 +0500

    📦New: Add Firebase Authentication

--- src/Component/Auth-Forms/Login.js
+++ src/Component/Auth-Forms/Login.js
@@ -1,18 +1,33 @@
 import React from 'react'
 import InputWrapper from '../ReusableWrapper/InputWrapper'
+import { signInWithEmailAndPassword } from 'firebase/auth';
+import { authApp } from '../../Firebase';
+import { Link } from 'react-router-dom';
 
 function Login() {
+    const login = async e => {
+        e.preventDefault();
+        try {
+            await signInWithEmailAndPassword(authApp, e.target.email.value, e.target.password.value);
+            await document.querySelector('#naiLink').click();
+        }
+        catch (e) {
+            alert(e.message);
+        }
+    }
+
     return (
-        <form className='flex flex-col justify-center items-center gap-2 h-screen px-2 py-5'>
+        <form onSubmit={login} className='flex flex-col justify-center items-center gap-2 h-screen px-2 py-5'>
             <h1 className='text-xl sm:text-2xl xl:text-3xl text-white font-bold tracking-wide' >Welcome Back</h1>
             <h4 className='text-sm text-[#55556b] font-medium tracking-wide mb-20' >Please Sign in to yor account</h4>
             <InputWrapper Type="email" Name="email" placeHolder="Email" />
             <InputWrapper Type="password" Name="password" placeHolder="Password" />
             <span className='text-[#70708b] text-xs text-right font-semibold w-5/6 sm:w-4/6 lg:w-1/2 tracking-wide' >Forget Password?</span>
             <div className='flex flex-col lg:flex-row gap-2 justify-between lg:gap-3 w-5/6 sm:w-4/6 lg:w-1/2 text-sm  mt-14'>
-                <button className='bg-btn-red text-white  p-3 font-semibold w-full xl:w-2/5 rounded-xl ' >Sign In</button>
+                <button type='submit' className='bg-btn-red text-white  p-3 font-semibold w-full xl:w-2/5 rounded-xl ' >Sign In</button>
                 <button className='bg-white text-black p-3 font-semibold w-full xl:w-2/5 rounded-xl' >Sign In with Google</button>
             </div>
+            <Link to="/homepage" id="naiLink" className="hidden">fd</Link>
         </form>
     )
 }
--- src/Component/Auth-Forms/Register.js
+++ src/Component/Auth-Forms/Register.js
@@ -1,9 +1,30 @@
 import React from 'react'
 import InputWrapper from '../ReusableWrapper/InputWrapper'
+import { createUserWithEmailAndPassword } from 'firebase/auth';
+import { authApp } from '../../Firebase';
+import { Link } from 'react-router-dom';
 
 function Register() {
+    const register = async e => {
+        e.preventDefault();
+        try {
+            if (e.target.password.value === e.target.cpassword.value) {
+                await createUserWithEmailAndPassword(authApp, e.target.email.value, e.target.password.value);
+                authApp.signOut();
+                document.querySelector('#naviLink').click();
+            }
+            else {
+                alert('Passwords do not match');
+            }
+        }
+        catch (e) {
+            alert(e.message);
+        }
+
+    }
+
     return (
-        <form className='flex flex-col justify-center items-center gap-2 h-screen px-2 py-5'>
+        <form onSubmit={register} className='flex flex-col justify-center items-center gap-2 h-screen px-2 py-5'>
             <h1 className='text-xl sm:text-2xl xl:text-3xl text-white font-bold tracking-wide ' >Create new account</h1>
             <h4 className='text-sm text-[#55556b] font-medium tracking-wide mb-20' >Please fill in the form to continue</h4>
             <InputWrapper Type="text" Name="fullname" placeHolder="Full Name" />
@@ -11,10 +32,10 @@ function Register() {
             <InputWrapper Type="password" Name="password" placeHolder="Password" />
             <InputWrapper Type="password" Name="cpassword" placeHolder="Confirm Password" />
             <div className='flex flex-col lg:flex-row lg:justify-between gap-2 lg:gap-3 w-5/6 sm:w-4/6 lg:w-1/2 text-sm mt-14 mb-3'>
-                <button className='bg-btn-red text-white p-3 font-semibold w-full xl:w-2/5 rounded-xl ' >Register</button>
+                <button type='submit' className='bg-btn-red text-white p-3 font-semibold w-full xl:w-2/5 rounded-xl ' >Register</button>
                 <button className='bg-white text-black p-3 font-semibold w-full xl:w-2/5 rounded-xl' >Register with Google</button>
             </div>
-            <h4 className="text-white text-xs xl:text-sm font-medium xl:w-1/2  tracking-wide mb-5">Already had an Account? <span className='text-btn-red' >Sign in</span></h4>
+            <h4 className="text-white text-xs xl:text-sm font-medium xl:w-1/2  tracking-wide mb-5">Already had an Account? <Link to='/signIn' id="naviLink" className='text-btn-red' >Sign in</Link></h4>
         </form>
     )
 }
