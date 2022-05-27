06382bd23e952260f6e25cb9578f2eb8de640027
meerhamzadev <hamzababar37@gmail.com>
2022-05-27 20:00:50 +0500

    🐛Fix: redundant actor names bug

--- src/Component/Cards/ActorCards.js
+++ src/Component/Cards/ActorCards.js
@@ -1,14 +1,15 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function ActorCards({ cardContent }) {
     return (
-        <section className="flex flex-col gap-0.5 items-center" id="card">
-            <div id="card-image" className="w-28 h-20 flex justify-center">
-                <img src={cardContent.image} className=" text-center rounded-full contained w-20 h-full " alt="something went wrong" />
+        <Link to={`/actor/${cardContent.id}`} className="flex flex-col gap-0.5 items-center md:gap-0" id="card">
+            <div id="card-image" className="w-28 h-20 sm:w-32 md:w-36 sm:h-28 md:h-32 lg:h-36 lg:w-42 flex justify-center">
+                <img src={cardContent.image} className=" text-center rounded-full contained w-20 sm:w-28 md:w-32 lg:w-36 h-full " alt="something went wrong" />
             </div>
-            <h3 className="font-semibold text-base tracking-wide leading-none">{cardContent.name}</h3>
-            <h4 className="font-light text-[#98979e] text-xs text-center">{cardContent.asCharacter}</h4>
-        </section>
+            <h3 className="font-semibold text-base tracking-wide leading-none xl:text-lg ">{cardContent.name}</h3>
+            <h4 className="font-light text-secondary-gray text-xs text-center">{cardContent.asCharacter.slice(0, (cardContent.asCharacter.length / 2))}</h4>
+        </Link>
     )
 }
 
--- src/Component/Cards/MovieCards.js
+++ src/Component/Cards/MovieCards.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import RatingsStars from './ChildComponent/Stars';
+import { Link } from 'react-router-dom';
 
-function Cards({ cardContent }) {
+function Cards({ cardContent, isPage }) {
     return (
-        <section className="flex flex-col gap-1" id="card">
-            <div id="card-image" className="rounded-xl w-44 h-56 ">
-                <img src={cardContent.image} className="rounded-xl contained w-full h-full " alt="something went wrong" />
+        <Link to={`/detail/${cardContent.id}`} className="flex flex-col gap-1" id="card">
+            <div id="card-image" className={`rounded-xl ${isPage ? "w-full" : "w-44 sm:w-48 md:w-72"} lg:w-full h-56 sm:h-60 md:h-72 lg:h-80 xl:h-88`}>
+                <img src={cardContent.image} className="rounded-xl w-full h-full " alt="something went wrong" />
             </div>
-            <h3 className="font-bold tracking-wide pl-1 ">{cardContent.title}</h3>
+            <h3 className="font-bold sm:font-semibold tracking-wide pl-1 xl:text-lg ">{cardContent.title}</h3>
             <RatingsStars Rating={cardContent.imDbRating} size={15} />
-        </section>
+        </Link>
     )
 }
 
