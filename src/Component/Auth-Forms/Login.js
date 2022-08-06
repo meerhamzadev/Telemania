4f093a8a4d6e75be850737ea40463e70ba6f0449
meerhamzadev <hamzababar37@gmail.com>
2022-08-06 23:18:50 +0500

    🐛 FIX: Login Bug

--- src/Component/Auth-Forms/Login.js
+++ src/Component/Auth-Forms/Login.js
@@ -18,14 +18,14 @@ function Login({ loginWithGoogle }) {
     }
 
     return (
-        <form className='flex flex-col justify-center items-center gap-2 h-screen px-2 py-5'>
+        <form onSubmit={login} className='flex flex-col justify-center items-center gap-2 h-screen px-2 py-5'>
             <h1 className='text-xl sm:text-2xl xl:text-3xl text-white font-bold tracking-wide' >Welcome Back</h1>
             <h4 className='text-sm text-[#55556b] font-medium tracking-wide mb-20' >Please Sign in to yor account</h4>
             <InputWrapper Type="email" Name="email" placeHolder="Email" />
             <InputWrapper Type="password" Name="password" placeHolder="Password" />
             <span className='text-[#70708b] text-xs text-right font-semibold w-5/6 sm:w-4/6 lg:w-1/2 tracking-wide' >Forget Password?</span>
             <div className='flex flex-col lg:flex-row gap-2 justify-between lg:gap-3 w-5/6 sm:w-4/6 lg:w-1/2 text-sm  mt-14'>
-                <button onClick={login} className='bg-btn-red text-white  p-3 font-semibold w-full xl:w-2/5 rounded-xl ' >Sign In</button>
+                <button type='submit' className='bg-btn-red text-white  p-3 font-semibold w-full xl:w-2/5 rounded-xl ' >Sign In</button>
                 <button onClick={loginWithGoogle} className='bg-white text-black p-3 font-semibold w-full xl:w-2/5 rounded-xl' >Sign In with Google</button>
             </div>
 
