5606aee74103adeeaa98f8bc08754a846ed493c8
meerhamzadev <hamzababar37@gmail.com>
2023-02-07 00:24:46 +0500

    🐛 FIX: Firebase configurations

--- src/Firebase.js
+++ src/Firebase.js
@@ -1,16 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
+import { collection, getFirestore } from 'firebase/firestore'
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyAI-lQYR8v2AaBzoNFaLVjyfXJrZ0TW14s",
-    authDomain: "telimania.firebaseapp.com",
-    projectId: "telimania",
-    storageBucket: "telimania.appspot.com",
-    messagingSenderId: "492195954500",
-    appId: "1:492195954500:web:0b78a50034e408d1cd6111"
+    apiKey: "AIzaSyDthkYA9ETQwnFw_sAwTafL2mTHFL61VQ0",
+    authDomain: "fir-136e0.firebaseapp.com",
+    projectId: "fir-136e0",
+    storageBucket: "fir-136e0.appspot.com",
+    messagingSenderId: "357614361193",
+    appId: "1:357614361193:web:fa7fbc4a1bdd6d947aa0f8"
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const authApp = getAuth(app);
+const db = getFirestore();
+export const movieDB = collection(db, 'movieDB');
